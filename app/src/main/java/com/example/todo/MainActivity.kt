51759af082e4e34e4e697efcbf96914744f7e686
ahmedhagag22 <https://github.com/ahmedhagag22>
2023-01-29 02:43:08 +0200

    initial project + Splash Screen + Home activity (2 fragment list , setting, floating button) + start dark mode

--- app/src/main/java/com/example/todo/MainActivity.kt
+++ app/src/main/java/com/example/todo/MainActivity.kt
@@ -1,11 +1,54 @@
 package com.example.todo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.fragment.app.Fragment
+import com.example.todo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var ViewBinding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ViewBinding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(ViewBinding.root)
+        selectedItem()
+        ViewBinding.bottomnavigation.selectedItemId=R.id.list
+
+    }
+    //func used at selected item in bottomnav
+    private  fun selectedItem()
+    {
+        ViewBinding.bottomnavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.list -> {
+                    showfragment(ListFragment())
+
+
+                }
+                R.id.setting -> {
+                    showfragment(SetteingFragment())
+                }
+
+
+            }
+            return@setOnItemSelectedListener true
+
+        }
+
+    }
+
+
+    //run the fragment
+    private  fun showfragment(Fragment: Fragment){
+        supportFragmentManager
+            .beginTransaction()
+            //framelayou > the place show on the fragment
+            .replace(R.id.framelayout_content,Fragment)
+            .commit()
+
+
     }
 }
